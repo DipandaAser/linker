c5a0d734fd5df704392d35bf775c98559c64be4b
Aser <aserdipanda@gmail.com>
2021-04-12 15:48:04 +0100

    Format commands description

--- commands.go
+++ commands.go
@@ -11,24 +11,24 @@ var commands = []Command{
 	{Text: "link", Description: "This command help you to link two groups/channels," +
 		" \nall messages created between this two groups/channels will be share in both two groups/channels" +
 		" \nPlease provide to Linker Code of those groups/channels." +
-		" \nExample: link yg64y iuh78" +
+		" \n\tExample: link yg64y iuh78" +
 		" \nPlease use this command only in private message with the bot" +
 		" \nDon't use in a group/channel because anyone can view your Linker Group/Channel Code"},
 	{Text: "diffuse", Description: "This command help diffuse/broadcast one Group/Channel to another Group/Channel," +
 		" \nonly the message of the first group/channel will be share on the second group/channel." +
 		" \nNote that with this command the oder of the groups/channels is important" +
 		" \nPlease provide to Linker Code of those groups/channels." +
-		" \nExample: diffuse yg64y iuh78" +
+		" \n\tExample: diffuse yg64y iuh78" +
 		" \nThe message of the group/channel yg64y will be share on iuh78. The reverse is not possible." +
 		" \nPlease use this command only in private message with the bot" +
 		" \nDon't use in a group because anyone can view your Linker Groups Code"},
 	{Text: "stop", Description: "Stop an active link or diffusion group/channel, " +
 		"Example: " +
-		"\n\t - Stop a specified link or diffusion" +
-		"\n\t stop yg64y" +
+		"\n\t- Stop a specified link or diffusion" +
+		"\n\t\tstop yg64y" +
 		"\n" +
-		"\n\t - Stop all active link or diffusion" +
-		"\n\t stop all"},
+		"\n\t- Stop all active link or diffusion" +
+		"\n\t\tstop all"},
 	{Text: "list", Description: "List all active links and diffusion of the group/channel"},
 }
 
